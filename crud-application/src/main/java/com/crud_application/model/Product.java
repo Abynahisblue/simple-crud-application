614b358e4103cf9ac598510570252cff6660b20c
ITCompliance <0719000427@ttu.edu.gh>
2024-08-21 13:50:31 +0000

    Add global exception handling

--- crud-application/src/main/java/com/crud_application/model/Product.java
+++ crud-application/src/main/java/com/crud_application/model/Product.java
@@ -4,6 +4,7 @@ package com.crud_application.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,7 +24,7 @@ public class Product {
     private String description;
 
     @NotNull(message = "Price cannot be empty")
-    @Min(value = 0, message = "Price must be greater than or equal to 0")
+    @Min(value = 1, message = "Price must be greater than or equal to 1")
     private double price;
 
     private int quantity;
